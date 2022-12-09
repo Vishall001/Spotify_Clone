703ecf6d15f6620ba60caed371fa9a789e2d1854
Vishall001 <vshmandloi@gmail.com>
2022-12-09 14:27:09 +0530

    created the genres in constants.js

--- spotify/src/pages/Discover.jsx
+++ spotify/src/pages/Discover.jsx
@@ -1,3 +1,12 @@
-const Discover = () => <div>Discover</div>;
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
+const Discover = () => {
+    console.log(genres)
+    return (
+        <div className="flex flex-col">
+
+        </div>
+    )
+}
 
 export default Discover;
