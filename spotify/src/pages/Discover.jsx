347745f23afb0f30d192f59cf35e36defb5b3e8b
Vishall001 <vshmandloi@gmail.com>
2022-12-10 23:57:57 +0530

    some changes

--- spotify/src/pages/Discover.jsx
+++ spotify/src/pages/Discover.jsx
@@ -1,12 +1,27 @@
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
 const Discover = () => {
-    console.log(genres)
-    return (
-        <div className="flex flex-col">
-
-        </div>
-    )
-}
+  const genreTitle = "Pop";
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10 ">
+        <h2 className="font-bold text-3xl text-white text-left">
+          Discover {genreTitle}
+        </h2>
+        <select
+          name=""
+          id=""
+          onChange={() => {}}
+          value=""
+          className="bg-black text-gray-300 p-3 rounded-lg text-sm outline-none sm:mt-0 mt-5"
+        >
+          {genres.map((genre) => (
+            <option>{genre.title}</option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+};
 
 export default Discover;
