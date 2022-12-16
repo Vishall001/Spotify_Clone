6bbb55d59bdce4c1d4b61a8dd187f0f3ca36d467
Vishall001 <vshmandloi@gmail.com>
2022-12-16 15:44:40 +0530

    fetch the top charts data

--- spotify/src/pages/Discover.jsx
+++ spotify/src/pages/Discover.jsx
@@ -1,6 +1,9 @@
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 const Discover = () => {
+  const {data,isFetching,error} = useGetTopChartsQuery();
+  console.log(data)
   const genreTitle = "Pop";
   return (
     <div className="flex flex-col">
@@ -16,9 +19,16 @@ const Discover = () => {
           className="bg-black text-gray-300 p-3 rounded-lg text-sm outline-none sm:mt-0 mt-5"
         >
           {genres.map((genre) => (
-            <option>{genre.title}</option>
+            <option key={genre.value} value={genre.value}>
+              {genre.title}
+            </option>
           ))}
         </select>
+      </div>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8 ">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+          <SongCard key={song.key} song={song} i={i} />
+        ))}
       </div>
     </div>
   );
---
+++ spotify/src/redux/services/shazamCore.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "53412bdcddmsh84ee38128c649f4p1a03f0jsnbca405a59fc8"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query:()=> "/charts/world" }),
+  }),
+});
+
+
+export const {
+    useGetTopChartsQuery ,
+}= shazamCoreApi
