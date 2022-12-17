eed0e58e3022c40840ba087cbd1a26b1782a7e8a
Vishall001 <vshmandloi@gmail.com>
2022-12-17 22:23:37 +0530

    Added the loader

--- spotify/src/components/Loader.jsx
+++ spotify/src/components/Loader.jsx
@@ -1,5 +1,10 @@
-const Loader = () => (
-  <div>Loader</div>
+ import loader from "../assets/loader.svg"
+const Loader = ({title}) => (
+  <div className="w-full justify-content items-center flex-col">
+    <img src={loader} alt="Loader" className="w-32 h-32 object-contain"
+     />
+     <h1 className="font-bold text-2xl text-white mt-2">{title || "Loading...."}</h1>
+  </div>
 );
 
 export default Loader;
--- spotify/src/pages/Discover.jsx
+++ spotify/src/pages/Discover.jsx
@@ -5,6 +5,10 @@ const Discover = () => {
   const {data,isFetching,error} = useGetTopChartsQuery();
   console.log(data)
   const genreTitle = "Pop";
+  if(isFetching)return <Loader title="Loading songs..."/>
+  if(error)return <Error/>
+
+  
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10 ">
@@ -26,7 +30,7 @@ const Discover = () => {
         </select>
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8 ">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+        {data ?.map((song, i) => (
           <SongCard key={song.key} song={song} i={i} />
         ))}
       </div>
