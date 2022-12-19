c5d990dddd3608fc0d7b455acda0c622c8ce6c24
Vishall001 <vshmandloi@gmail.com>
2022-12-19 16:57:24 +0530

    created top charts section

--- spotify/src/components/TopPlay.jsx
+++ spotify/src/components/TopPlay.jsx
@@ -10,7 +10,24 @@ import "swiper/css";
 import "swiper/css/free-mode";
 
 const TopChartCard = ({ song, i }) => (
-  <div className="w-full flex -flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2"></div>
+  <div className="w-full flex -flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
+    <h3 className="font-bold text-base text-white mr-3">{i + 1}</h3>
+    <div className="flex-1 flex flex-row justify-between item-center">
+      <img
+        className="w-20 h-20 rounded-lg"
+        src={song?.images?.coverart}
+        alt={song?.title}
+      />
+      <div className="flex-1 flex flex-col justify-center mx-3">
+        <Link to={`/songs/${song.key}`}>
+          <p className="text-xl font-bold text-white">{song?.title}</p>
+        </Link>
+        <Link to={`/artists/${song?.artists[0].adamid}`}>
+          <p className="text-ase text-gray-300 mt-1">{song?.subtitle}</p>
+        </Link>
+      </div>
+    </div>
+  </div>
 );
 
 const TopPlay = () => {
