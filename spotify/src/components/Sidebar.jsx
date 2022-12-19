2b5a8368b00403bdedd35928abfd8f5155c84bb7
Vishall001 <vshmandloi@gmail.com>
2022-12-19 10:13:08 +0530

    display the logo on side bar

--- spotify/src/components/Sidebar.jsx
+++ spotify/src/components/Sidebar.jsx
@@ -1,5 +1,18 @@
-const Sidebar = () => (
-  <div>Sidebar</div>
-);
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { RiCloseLine } from "react-icons/ri";
+import spotify from "../assets/spotify.svg";
+import { links } from "../assets/constants";
+
+const Sidebar = () => {
+  const { mobileMenuOpen, setMobileMenuOpen } = useState(false);
+  return (
+    <>
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+        <img src={spotify} alt="Logo" className="w-full h-14 object-contain" />
+      </div>
+    </>
+  );
+};
 
 export default Sidebar;
