a3d47efe010449d9a3efc329dbb961354b569bb2
Vishall001 <vshmandloi@gmail.com>
2022-12-19 10:18:52 +0530

    added the links in sidebar

--- spotify/src/components/Sidebar.jsx
+++ spotify/src/components/Sidebar.jsx
@@ -4,12 +4,29 @@ import { RiCloseLine } from "react-icons/ri";
 import spotify from "../assets/spotify.svg";
 import { links } from "../assets/constants";
 
+const NavLinks = ({ handleClick }) => (
+  <div className="mt-10">
+    {links.map((item) => (
+      <NavLink
+        key={item.name}
+        to={item.to}
+        className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400  "
+        onClick={() => handleClick && handleClick()}
+      >
+        <item.icon className="w-6 h-6 mr-2" />
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
+);
+
 const Sidebar = () => {
   const { mobileMenuOpen, setMobileMenuOpen } = useState(false);
   return (
     <>
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
         <img src={spotify} alt="Logo" className="w-full h-14 object-contain" />
+        <NavLinks />
       </div>
     </>
   );
