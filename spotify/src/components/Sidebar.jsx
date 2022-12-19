312cc799f04d1a4a0f5d233cd32bf4a846ba9edd
Vishall001 <vshmandloi@gmail.com>
2022-12-19 10:51:36 +0530

    Added the menu button

--- spotify/src/components/Sidebar.jsx
+++ spotify/src/components/Sidebar.jsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import { RiCloseLine } from "react-icons/ri";
 import spotify from "../assets/spotify.svg";
 import { links } from "../assets/constants";
+import { HiOutlineMenu } from "react-icons/hi";
 
 const NavLinks = ({ handleClick }) => (
   <div className="mt-10">
@@ -21,12 +22,33 @@ const NavLinks = ({ handleClick }) => (
 );
 
 const Sidebar = () => {
-  const { mobileMenuOpen, setMobileMenuOpen } = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
     <>
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
         <img src={spotify} alt="Logo" className="w-full h-14 object-contain" />
         <NavLinks />
+      </div>
+      <div className="absolute md:hidden block top-6 right-3">
+        {mobileMenuOpen ? (
+          <RiCloseLine
+            className="w-6 h-6 text-white mr-2"
+            onClick={() => setMobileMenuOpen(false)}
+          />
+        ) : (
+          <HiOutlineMenu
+            className="w-6 h-6 text-white mr-2"
+            onClick={() => setMobileMenuOpen(true)}
+          />
+        )}
+      </div>
+      <div
+        className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[$483d8d] backdrop-blur-lg z-10 p-6 md-hidden smooth-transition ${
+          mobileMenuOpen ? "left-0" : "-left-full"
+        }`}
+      >
+        <img src={spotify} alt="Logo" className="w-full h-14 object-contain" />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
     </>
   );
