3869899dc56f5f9b4c1bce6bddc73e2e8d7494f7
Vishall001 <vshmandloi@gmail.com>
2022-12-20 12:13:35 +0530

    showen the related songs

--- spotify/src/components/RelatedSongs.jsx
+++ spotify/src/components/RelatedSongs.jsx
@@ -6,10 +6,23 @@ const RelatedSongs = ({
   activeSong,
   handlePauseClick,
   handlePlayClick,
+  artistId,
 }) => {
   return (
     <div className="flex flex-col">
       <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
+      <div className="mt-6 w-full flex flex-col ">
+        {data?.map((song, i) => (
+          <SongBar
+            key={`${song.key}-${artistId}`}
+            song={song}
+            i={i}
+            artistId={artistId}
+            handlePauseClick={handlePauseClick}
+            handlePlayClick={handlePlayClick}
+          />
+        ))}
+      </div>
     </div>
   );
 };
