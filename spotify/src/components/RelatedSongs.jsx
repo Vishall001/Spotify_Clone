a6bb3cdfafb74bd43271e3ca4f1b07f409ae437f
Vishall001 <vshmandloi@gmail.com>
2022-12-20 11:44:33 +0530

    fetch related songs data and also write the code for songBar

--- spotify/src/components/RelatedSongs.jsx
+++ spotify/src/components/RelatedSongs.jsx
@@ -1,5 +1,17 @@
-const RelatedSongs = () => (
-  <div>Loader</div>
-);
+import SongBar from "./SongBar";
+
+const RelatedSongs = ({
+  data,
+  isPlaying,
+  activeSong,
+  handlePauseClick,
+  handlePlayClick,
+}) => {
+  return (
+    <div className="flex flex-col">
+      <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
+    </div>
+  );
+};
 
 export default RelatedSongs;
