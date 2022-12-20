79d575a27062adb7dac9e5839f803f02f0b53410
Vishall001 <vshmandloi@gmail.com>
2022-12-20 16:07:43 +0530

    Added the top charts data

--- spotify/src/pages/AroundYou.jsx
+++ spotify/src/pages/AroundYou.jsx
@@ -25,7 +25,7 @@ const AroundYou = () => {
   return (
     <div className="flex flex-col ">
       <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
-        Around You
+        Around You <span className="font-black">{country}</span>
       </h2>
       <div className="flex flex-wrap  sm:justify-start justify-center gap-8">
         {data?.map((song, i) => (
