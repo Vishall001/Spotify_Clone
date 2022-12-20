51dbb6fbb6aac941a3cafccbaea9d5f2c34bd9ec
Vishall001 <vshmandloi@gmail.com>
2022-12-20 11:29:53 +0530

    added the subtitle and genres

--- spotify/src/components/DetailsHeader.jsx
+++ spotify/src/components/DetailsHeader.jsx
@@ -19,8 +19,20 @@ const DetailsHeader = ({ artistId, artistData, songData }) => {
           <p className="font-bold sm:text-3xl text-xl text-white">
             {artistId ? artist?.name : songData?.title}
           </p>
+          {!artistId && (
+            <Link to={`artistId/${songData?.artists[0].adamid}`}>
+              <p className="text-base text-gray-400 mt-2">
+                {" "}
+                {songData?.subtitle}
+              </p>
+            </Link>
+          )}
+          <p className="text-base text-gray-400 mt-2">
+            {artistId ? artist?.genreNames[0] : songData?.genres?.primary}
+          </p>
         </div>
       </div>
+      <div className="w-full sm:h-44 h-24" />
     </div>
   );
 };
